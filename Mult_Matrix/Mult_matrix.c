4b9644aa38959b270dd227eae37b66a548b81ba2
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-02-23 12:44:32 -0500

    change branch

--- Mult_Matrix/Mult_matrix.c
+++ Mult_Matrix/Mult_matrix.c
@@ -4,26 +4,32 @@
 #include <omp.h>
 #define CHUNKSIZE 20
 
+void prin_matrix(float **matriz ,int row ,int col){
+  for (int i = 0; i < col; ++i)
+  {
+    for (int j = 0; j < row; ++j)
+    {
+      printf("%.2f %s", matriz[i][j] , " " );
+    }
+  printf("\n");
+  }
+}
+
 void mult_matriz(float **matriz1 , float **matriz2 , float **matrizR, int col1, int  row1, int col2, int row2 ){
 	int tid, chunk , i , j , k;
   chunk = CHUNKSIZE;
   float cont;
-
-  #pragma omp parallel shared(matriz1,matriz2,matrizR,chunk) private(tid,i,cont,col1,col2,row1,row2)
+  #pragma omp parallel shared(matriz1,matriz2,matrizR,chunk) private(tid,i,j,k)
   {
     #pragma omp for schedule(dynamic,chunk)
   	for (i = 0; i < col1; ++i){
-  		printf("%s", "[" );
   	    for (j = 0; j < row2; ++j){
   	      cont = 0,0;
   	      for (k = 0; k < row2; ++k){
   	        cont += matriz1[i][k] * matriz2[k][j];
   	      }
   	      matrizR[i][j] = cont;
-  	      printf("%.2f ", matrizR[i][j]);
   	    }
-  	    printf("%s", "]" );
-  	    printf("\n");
   	  }
     }
   }
@@ -58,15 +64,11 @@ void matrix_dinamic(int col , int row , float **matriz ){
         printf ("Error de Mem.");
     for (int i = 0; i < row; ++i)
     {
-    	printf("%s", "[" );
     	for (int j = 0; j < col; ++j)
     	{	
     		float random = rand()%100;
     		matriz[i][j] = random;
-    		printf("%.2f ", matriz[i][j] );
     	}
-    	printf("%s", "]" );
-    	printf("\n");
     }
 
 }
@@ -100,12 +102,13 @@ void save_matriz(float **Matrix, int row, int col){
 int main(int argc, char const *argv[])
 {
 
-	int flag;
+	   int flag;
     int Scol1;
     int Srow1;
     int Scol2;
     int Srow2;
 
+    clock_t time_ini,time_end;
     printf("%s\n\n", "Multiplicación de matrices" );
 	printf("Introduzca el numero de columnas matriz 1: ");
     scanf("%d", &Scol1);
@@ -140,15 +143,21 @@ int main(int argc, char const *argv[])
 
 		printf("%s\n", "-----------Matriz 1----------" );
 		matrix_dinamic(Scol1 , Srow1 , matriz1);
+    prin_matrix(matriz1,Srow1,Scol1);
 		printf("%s\n", "-----------Matriz 2----------" );
 		matrix_dinamic(Scol2 , Srow2 , matriz2);
+    prin_matrix(matriz2,Srow2,Scol2);
 		printf("%s\n","------------Resultado Multiplicación-----------" );
 		fill_matriz(Scol2 , Srow1 , matrizR);
+    time_ini = clock();
 		mult_matriz(matriz1,matriz2,matrizR,Scol1,Srow1,Scol2,Srow2);
+    time_end = clock();
+    prin_matrix(matrizR,Srow1,Scol2);
 		save_matriz(matrizR , Srow1 , Scol2);
     free(matriz1);
     free(matriz2);
     free(matrizR);
+    printf("%s %.6f\n", "Tiempo de ejecución: ", (double) (time_end - time_ini)/CLOCKS_PER_SEC );
     	
     }
 	
