6527a280827e0407177f409ef57963092ba73b03
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-02-28 15:33:21 -0500

    add omp examples

---
+++ omp_examples/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=omp_hello_world_jorsh
+#SBATCH --output=res_omp_hello_world.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+./omp_hello
+
