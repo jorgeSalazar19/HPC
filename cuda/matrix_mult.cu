f67b2f93a8b3941cc654f9df6263f4db0bf2c240
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-04-12 19:58:20 -0500

    add changes

--- cuda/matrix_mult.cu
+++ cuda/matrix_mult.cu
@@ -46,7 +46,7 @@ void read_matrix_from_file(float *M, FILE *archivo, int rows, int cols){
 	return;
 }
 
-void validate(int colM1 ,int rowM2){
+bool validate(int colM1 ,int rowM2){
 
 	if (colM1 != rowM2){
 		return true;
